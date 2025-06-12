ed7aea7a0f4e1910712186cc1020817ac6d15622
Adam <122129206+Amar-Adam-BENADJAOUD@users.noreply.github.com>
2025-06-12 11:00:50 +0200

    Update test_obstacles.py

--- test_obstacles.py
+++ test_obstacles.py
@@ -1,2 +1,41 @@
-import test_servo, test_motor, test_ultra
+import test_ultra, test_deplacement, time
 
+DISTANCE = 200
+SPEED = 100
+ANGLE = 5
+INTER = 0.01
+
+def evitement():
+    # Move forward until an obstacle is detected
+    while(test_ultra.ultrasonic_distance() > DISTANCE):
+        test_deplacement.forward(SPEED)
+
+    # Avoid the obstacle and go back into your initial sens
+    for i in range(10):
+        test_deplacement.turnleft(ANGLE * i)
+        time.sleep(INTER)
+    test_deplacement.forward(SPEED)
+    time.sleep(1)
+    for i in range(10):
+        test_deplacement.turnright(ANGLE * i)
+        time.sleep(INTER)
+
+    # Move forward until overtake the obstacle
+    test_deplacement.forward(SPEED)
+    time.sleep(5)
+
+
+    # Go back into your initial path
+    for i in range(10):
+        test_deplacement.turnright(ANGLE * i)
+        time.sleep(INTER)
+    test_deplacement.forward(SPEED)
+    time.sleep(1)
+    for i in range(10):
+        test_deplacement.turnleft(ANGLE * i)
+        time.sleep(INTER)
+
+
+while(True) :
+    while(1):
+        evitement()
